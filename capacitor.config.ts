2c37bbf7d1c37010a9f5155ec876d37c2b87521f
Noken29 <42448187+Noken29@users.noreply.github.com>
2023-05-30 12:28:17 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examination-work',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
